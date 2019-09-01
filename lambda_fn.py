b05ae8828cf6e8daca2a0db57b4962a83faf1ed6
saikat519 <saikat.mukhopadhyay17@gmail.com>
2019-09-01 18:53:43 +0530

    DONE

---
+++ lambda_fn.py
@@ -0,0 +1,22 @@
+'''
+lambda functions are those who's have no name
+it's syntax is 
+x=lambda a,b,c: (operation on abc)
+then print(x)
+'''
+
+
+x=lambda a,b,c: a+b+c
+
+print(x(10,20,30))
+
+
+y=lambda a,b: a*b
+
+print(y(100,100))
+
+
+
+z=lambda a,b: a/b
+
+print(z(10,8))
